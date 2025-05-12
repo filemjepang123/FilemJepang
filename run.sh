02bd644cb7ad8a587688ef5fe74b658379a7b3e1
filemjepang123 <filemjepang123@gmail.com>
2025-05-13 05:39:05 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+
+
+read -p "Masukkan file video (.mp4): " video
+read -p "Masukkan durasi awal pemotongan video: " dua
+read -p "Masukkan durasi akhir pemotongan video: " dui
+
+echo "Sedang dikerjakan..."
+sleep 3
+
