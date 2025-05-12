2711d817ff3dd25915a2a47b2288a4bf08e547c7
filemjepang123 <filemjepang123@gmail.com>
2025-05-13 05:41:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,12 @@ read -p "Masukkan durasi awal pemotongan video: " dua
 read -p "Masukkan durasi akhir pemotongan video: " dui
 
 echo "Sedang dikerjakan..."
+
+echo "Membuat CODE..."
 sleep 3
+
+nama_file_output=""
+
+python3 create_code.py > "${nama_file_output}"
+echo "${nama_file_output}"
 
